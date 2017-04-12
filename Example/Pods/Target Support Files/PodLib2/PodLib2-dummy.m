c563d99c31cd19e7f1e9fe443f917cad44cb5f1d
钱志义 <zhiyi@qingfengweb.com>
2017-04-12 15:53:05 +0800

    first commit

---
+++ Example/Pods/Target Support Files/PodLib2/PodLib2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodLib2 : NSObject
+@end
+@implementation PodsDummy_PodLib2
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodLib2_Example/Pods-PodLib2_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodLib2_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PodLib2_Example
+@end
